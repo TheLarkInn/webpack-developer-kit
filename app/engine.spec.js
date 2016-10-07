193e956f4c3128195851a75e6493863d6a9ef348
Sam <zhao.shengjun@gmail.com>
2016-10-07 18:54:49 +1000

    unit testing and e2e testing for dev script

---
+++ app/engine.spec.js
@@ -0,0 +1,19 @@
+import {V6Engine, V8Engine, getVersion} from './engine';
+
+describe('Engine - ',() => {
+ it('should have a v6 engine', () => {
+   let v6 = new V6Engine();
+   expect(v6.toString()).toBe('V6');
+ })
+
+ it('should have a v8 engine', () => {
+   let v8 = new V8Engine();
+   expect(v8.toString()).toBe('V8');
+ })
+
+ it('should get version', () => {   
+   expect(getVersion()).toBe('1.0');
+ })
+
+  
+})
