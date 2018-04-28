ae92a94340a390549f5d0ca9af4bc8ca26075387
Sean Larkin <sean.larkin@cuw.edu>
2018-04-28 03:04:27 -0500

    feat(webpack4): upgrade the developer kit to webpack4

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,41 @@
 'use strict';
-const webpack = require('webpack');
-const webpackSources = require('webpack-sources');
-const enhancedResolve = require('enhanced-resolve');
+// Plugins and extra supporting libs for webpack you might want to extend in your labratory! 😍
+// const webpack = require('webpack');
+// const memoryFs = require('memory-fs');
+// const webpackSources = require('webpack-sources');
+// const enhancedResolve = require('enhanced-resolve');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
-
+/**
+ * @description
+ * Typedef's so that you can access type
+ * intellisense for each class and instance
+ * inside of your hooks and parameters
+ *
+ * Use @typedef and `import("webpack/lib/WhateverClass")` if you want to pull in a class and its properties from webpack!!!
+ *
+ */
+const welcome = 'webpack-developer-kit';
+console.log(welcome);
+/** @typedef {import("webpack/lib/Compiler")} Compiler */
+/** @typedef {import("webpack/lib/Compilation")} Compilation */
+/** @typedef {import("webpack/lib/NormalModule")} NormalModule */
+/** @typedef {import("webpack/lib/ContextModule")} ContextModule */
+/** @typedef {import("webpack/lib/NormalModuleFactory")} NormalModuleFactory */
+/** @typedef {import("webpack/lib/ContextModuleFactory")} ContextModuleFactory */
+/** @typedef {import("webpack/lib/Module")} Module */
+/** @typedef {import("webpack/lib/Chunk")} Chunk */
+/** @typedef {import("webpack/lib/ChunkGroup")} ChunkGroup */
+/** @typedef {import("webpack/lib/Dependency")} Dependency */
+/** @typedef {{normalModuleFactory: NormalModuleFactory, contextModuleFactory: ContextModuleFactory, compilationDependencies: Set<Dependency>}} CompilationParams */
 
 module.exports = {
   entry: {
-    'car': ['./app/car.js'],
+    car: ['./app/car.js'],
   },
   output: {
-    path: path.join(__dirname,'/dist'),
+    path: path.join(__dirname, '/dist'),
     filename: '[name].bundle.js',
   },
   module: {
@@ -24,10 +47,7 @@ module.exports = {
         test: /\.js$/,
         oneOf: [
           {
-            loaders: [
-              'babel-loader',
-              'custom-loader',
-            ],
+            loaders: ['babel-loader', 'custom-loader'],
           },
         ],
       },
@@ -44,23 +64,53 @@ module.exports = {
   plugins: [
     // This function is the `apply` function if you were to create an external plugin
     // Having it inline provides some nice conviences for debugging and development
-    function() {
+    function apply() {
+      /** @type {Compiler} */
       var compiler = this;
-      compiler.plugin('compilation', function(compilation) {
-        compilation.plugin('after-optimize-modules', function(modules) {
-          // debugger;
-        });
-        compilation.plugin('after-optimize-chunks', function(chunks) {
-          // debugger;
-        });
-      });
+      compiler.hooks.compilation.tap('MyCustomInlinePlugin', compilationTapFunction);
     },
     new HtmlWebpackPlugin({
       template: './app/index.html',
     }),
   ],
-  resolve: {
-    extensions: ['.js'],
-  },
   devtool: false,
+  mode: 'none',
 };
+
+/**
+ * @param {Compilation} compilation
+ * @param {CompilationParams} params
+ */
+function compilationTapFunction(compilation, { normalModuleFactory, contextModuleFactory, _compilationDependencies }) {
+  compilation.hooks.afterOptimizeModules.tap('MyCustomInlinePlugin', modulesTapFunction);
+  compilation.hooks.afterOptimizeChunks.tap('MyCustomInlinePlugin', chunksTapFunction);
+
+  // Inline Tap Functions for ModuleFactories
+  normalModuleFactory.hooks.beforeResolve.tapAsync('MyCustomInlinePlugin', (_data, cb) => {
+    // debugger;
+    cb();
+  });
+  contextModuleFactory.hooks.afterResolve.tapAsync('MyCustomInlinePlugin', (_data, cb) => {
+    // debugger;
+    cb();
+  });
+}
+
+/**
+ * `afterOptimizeModules` tap function
+ * @param {(Module|ContextModule|NormalModule)[]} _modules
+ */
+function modulesTapFunction(_modules) {
+  // debugger;
+  // uncomment the statement above and run the debug script to explore the data in this function/hook/tap
+}
+
+/**
+ * `afterOptimizeModules` tap function
+ * @param {Chunk[]} _chunks
+ * @param {ChunkGroup[]} _chunkGroups
+ */
+function chunksTapFunction(_chunks, _chunkGroups) {
+  // debugger;
+  // uncomment the statement above and run the debug script to explore the data in this function/hook/tap
+}
